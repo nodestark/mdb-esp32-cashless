4a5045661bec40af960106ef181dc0c8df1f8167
leonardo soares de souza <leonardobsi@gmail.com>
2025-12-24 11:28:58 -0300

    Docker VMflow.xyz

---
+++ Docker/volumes/functions/request-credit/index.ts
@@ -0,0 +1,68 @@
+// deno run --allow-net index.ts
+import { createClient } from 'jsr:@supabase/supabase-js@2'
+import { decodeBase64, encodeBase64 } from 'jsr:@std/encoding/base64'
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE";
+
+function toScaleFactor(p: number, x: number, y: number): number {
+  return p / x / Math.pow(10, -y);
+}
+
+function fromScaleFactor(p: number, x: number, y: number): number {
+  return p * x * Math.pow(10, -y);
+}
+
+Deno.serve(async (req) => {
+
+    try {
+        const body = await req.json();
+
+        const payload: Uint8Array = decodeBase64(body.payload)
+
+        const supabase = createClient( "https://supabase.vmflow.xyz", SUPABASE_ANON_KEY,
+            { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+        )
+
+        const { data: embeddedData, error } = await supabase.from("embeddeds").select("passkey,subdomain,id").eq("subdomain", body.subdomain);
+
+        const passkey: number[] = [...embeddedData[0].passkey].map(c => c.charCodeAt(0));
+
+        for(let k= 0; k < passkey.length; k++){
+            payload[k + 1] ^= passkey[k];
+        }
+        let chk = payload.slice(0, -1).reduce((acc, val) => acc + val, 0);
+
+        if( payload[payload.length - 1] == (chk & 0xff) ){
+
+            const itemPrice = (payload[2] << 8) | (payload[3] << 0);
+            const itemNumber = (payload[4] << 8) | (payload[5] << 0);
+
+            await supabase.from("sales").insert([{ 
+            	embedded_id: embeddedData[0].id,
+            	item_number: itemNumber, 
+            	item_price: fromScaleFactor(itemPrice, 1, 2), 
+            	channel: "ble",
+            	lat: (body.lat !== undefined ? body.lat : null),
+		lng: (body.lng !== undefined ? body.lng : null) }])
+
+            payload[0] = 0x03;
+
+            let chk_ = payload.slice(0, -1).reduce((acc, val) => acc + val, 0);
+            payload[payload.length - 1] = chk_;
+
+            for(let k= 0; k < passkey.length; k++){
+                payload[k + 1] ^= passkey[k];
+            }
+
+            return new Response(JSON.stringify({payload: encodeBase64(payload)}), { headers: { 'Content-Type': 'application/json' } })
+        }
+        return new Response(JSON.stringify({ }), { headers: { 'Content-Type': 'application/json' } })
+
+    } catch (err) {
+        return new Response(JSON.stringify({ message: err?.message ?? err }), {
+            headers: { 'Content-Type': 'application/json' },
+            status: 500
+        })
+    }
+})
+
---
+++ Docker/volumes/mqtt/clients/mqtt_domain.py
@@ -0,0 +1,76 @@
+# paho-mqtt
+# supabase
+
+import re
+import paho.mqtt.client as mqtt
+from supabase import create_client, Client
+
+from datetime import datetime, timezone
+import time
+
+role_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJzZXJ2aWNlX3JvbGUiLAogICAgImlzcyI6ICJzdXBhYmFzZS1kZW1vIiwKICAgICJpYXQiOiAxNjQxNzY5MjAwLAogICAgImV4cCI6IDE3OTk1MzU2MDAKfQ.DaYlNEoUrrEn2Ig7tqibS-PHK5vgusbcbo7X36XVt4Q"
+
+supabase: Client = create_client("https://supabase.vmflow.xyz", role_key)
+
+def to_scale_factor(p: float, x: float, y: float) -> float:
+    return p / x / (10 ** -y)
+
+def from_scale_factor(p: float, x: float, y: float) -> float:
+    return p * x * (10 ** -y)
+
+def on_connect(client, userdata, flags, rc):
+
+    if rc == 0:
+        print(" Conectado ao broker!")
+        client.subscribe("/domain/+/sale")
+        client.subscribe("/domain/+/status")
+    else:
+        print(f" Falha na conexão. Código: {rc}")
+
+def on_message(client, userdata, msg):
+    try:
+        match = re.match(r"^/domain/(\d+)/(sale|status)$", msg.topic)
+        if match:
+            domain_id = int(match.group(1))
+            event_type = match.group(2) # "sale" or "status"
+            payload = bytearray(msg.payload)
+            # print(event_type)
+
+            if event_type == "status":
+                supabase.table("embeddeds").update({"status_at": datetime.now(timezone.utc).isoformat(), "status": payload.decode('utf-8', errors='ignore')}).eq("subdomain", domain_id).execute()
+
+            if event_type == "sale":
+                res = supabase.table("embeddeds").select("passkey,subdomain,id,owner_id").eq("subdomain", domain_id).execute()
+
+                embedded = res.data[0]
+                passkey = [ord(c) for c in embedded["passkey"]]
+
+                for k in range(len(passkey)):
+                    payload[k + 1] ^= passkey[k]
+
+                chk = sum(payload[:-1])
+                if payload[-1] == (chk & 0xFF):
+
+                    timestampSec = ((payload[6] << 24) |
+                                    (payload[7] << 16) |
+                                    (payload[8] << 8) |
+                                    (payload[9] << 0))
+
+                    current_time = int(time.time())
+
+                    if abs(current_time - timestampSec) <= 8:
+
+                        item_price = (payload[2] << 8) | (payload[3] << 0)
+                        item_number = (payload[4] << 8) | (payload[5] << 0)
+
+                        supabase.table("sales").insert([{"owner_id":embedded["owner_id"],"embedded_id": embedded["id"],"item_number": item_number,"item_price": from_scale_factor(item_price, 1, 2),"channel": "cash"}]).execute()
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("mqtt.vmflow.xyz", 1883, keepalive=60)
+client.loop_forever()
+
