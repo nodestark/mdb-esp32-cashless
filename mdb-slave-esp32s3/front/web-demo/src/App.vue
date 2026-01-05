6be8a97767e74e9ea44580930975a5d0451dc1ec
Lucien Kerl <lucien@kerl.io>
2026-01-05 01:42:14 +0100

    added basic WebUI to get information about the device

---
+++ mdb-slave-esp32s3/front/web-demo/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" fixed app clipped>
+      <v-list dense>
+        <v-list-tile to="/">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="red accent-4" dark fixed app clipped-left>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>VMFlow</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer color="red accent-4" app fixed>
+      <span class="white--text">&copy; VMFlow. All rights reserved.</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      drawer: null
+    };
+  }
+};
+</script>
